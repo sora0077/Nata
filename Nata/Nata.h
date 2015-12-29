29ff8c951a51594878429237d9f6aeba363a6dae
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-12-30 04:47:07 +0900

    Initial Commit

---
+++ Nata/Nata.h
@@ -0,0 +1,19 @@
+//
+//  Nata.h
+//  Nata
+//
+//  Created by 林達也 on 2015/12/30.
+//  Copyright © 2015年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nata.
+FOUNDATION_EXPORT double NataVersionNumber;
+
+//! Project version string for Nata.
+FOUNDATION_EXPORT const unsigned char NataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nata/PublicHeader.h>
+
+
